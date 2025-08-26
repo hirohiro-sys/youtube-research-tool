7a19144733c9eb5a11351f6339632aeba7422298
hirohiro-sys <katohiro8843@icloud.com>
2025-08-26 19:19:48 +0900

    footerの位置を修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Header />
-        {children}
+        <main className="min-h-[100vh]">{children}</main>
         <Footer />
       </body>
     </html>
